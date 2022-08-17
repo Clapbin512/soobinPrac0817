2468c305f2127334186855605ea9f6b2116975b5
soobin <clapbin512@gmail.com>
2022-08-17 17:00:10 +0900

    Initial Commit

---
+++ soobinPrac0817/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  soobinPrac0817
+//
+//  Created by Soobin Park on 2022/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+//1
+
+}
+
